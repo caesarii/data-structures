d0fc59519553734706b50eff7da192a517671b87
qinghe <qinghe@2dfire.com>
2018-01-27 13:48:09 +0800

    main

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "./test/testlist.c"
+//#include "./test/testqueue.c"
+//#include "./test/testhashtable.c"
+//#include "./test/teststack.c"
+
+int
+main(int argc, const char * argv[]) {
+
+//    testList();
+//    testStack();
+//    testQueue();
+//    testHashTable();
+
+    // 返回 0 表示程序执行完毕并成功退出
+    return 0;
+}
+
