9475ca13abcf6515eac3a8898f367cfd4dbd7111
qinghe <qinghe@2dfire.com>
2018-01-27 13:48:29 +0800

    stack 实现

---
+++ Stack.c
@@ -0,0 +1,59 @@
+
+#include "Stack.h"
+
+struct StackStruct {
+    List *list;
+};
+
+Stack *
+StackCreate(void) {
+    Stack *s = malloc(sizeof(Stack));
+    List *l = ListCreate(NULL, 0);
+    s->list = l;
+    return s;
+}
+
+void
+StackLog(Stack *stack) {
+    ListLog(stack->list);
+}
+
+int
+StackLength(Stack *stack) {
+     return ListLength(stack->list);
+}
+
+
+void StackPush(Stack *stack, type element){
+    ListPrepend(stack->list, element);
+};
+
+int
+StackPop(Stack *stack) {
+    int deleted = ListShift(stack->list);
+    return deleted;
+}
+
+
+bool
+StackIsEmpty(Stack *stack) {
+    if(ListLength(stack->list) == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void
+StackClear(Stack *stack) {
+    while(!StackIsEmpty(stack)) {
+        StackPop(stack);
+    }
+}
+
+void
+StackRemove(Stack *stack){
+    StackClear(stack);
+    free(stack);
+}
+
