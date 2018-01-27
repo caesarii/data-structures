ad64ebc22aee726abdb058f95b33d5512b05fa40
qinghe <qinghe@2dfire.com>
2018-01-27 13:56:44 +0800

    main update

--- main.c
+++ main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include "./test/testlist.c"
-//#include "./test/testqueue.c"
-//#include "./test/testhashtable.c"
-//#include "./test/teststack.c"
+#include <stdlib.h>
 
-int
-main(int argc, const char * argv[]) {
 
-//    testList();
-//    testStack();
-//    testQueue();
-//    testHashTable();
-
-    // 返回 0 表示程序执行完毕并成功退出
+int main()
+{
+    testList();
+    testQueue();
+    testStack();
+    testHashTable();
+    printf("Hello world!\n");
     return 0;
 }
-
